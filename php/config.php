39af5b10dd47dd411d07df1b9a6358591fa568bb
TomokoYamada <53735102+TomokoYamada@users.noreply.github.com>
2019-12-18 22:11:50 +0900

    Add files via upload

---
+++ php/config.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors', 1);
+
+define('DSN','mysql:host=localhost;dbname=order_system');
+define('DB_USERNAME','user');
+define('DB_PASSWORD','password');
+
+session_start();
+?>
